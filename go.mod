1e87ff4c2f1d3fa750ff5225b87a85ac9aba74b6
Kengo Suzuki <kengoscal@gmail.com>
2019-06-09 00:04:08 +0900

    fixing stupid initial error

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ken5scal/oauth-az
+
+require (
+	github.com/gorilla/handlers v1.4.0
+	github.com/gorilla/mux v1.7.2
+	github.com/pelletier/go-toml v1.4.0
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/pelletier/go-toml"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var oauthConfig oauth2.Config
+var port string
+
+func init() {
+	tomlInBytes, err := ioutil.ReadFile("config.toml")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("failed reading a toml file: %v", err.Error()))
+	}
+
+	config, err := toml.LoadBytes(tomlInBytes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("failed parsing a toml file: %v", err.Error()))
+	}
+
+	port = strconv.FormatInt(config.Get("port").(int64), 10)
+}
+
+func main() {
+	// assuming administrative requests from frontend
+	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type"})
+	// Options is for CORS preflight
+	allowedMethods := handlers.AllowedMethods([]string{http.MethodOptions, http.MethodGet, http.MethodPost})
+
+	r := mux.NewRouter()
+	//r.HandleFunc("/hoge", fuga)
+	srv := &http.Server {
+		Addr: "localhost:" + port,
+		Handler: handlers.CORS(allowedOrigins, allowedHeaders, allowedMethods)(r),
+	}
+	log.Fatal(srv.ListenAndServe())
+
+}
