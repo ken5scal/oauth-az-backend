931d16d94d59e65d55fca002789e6f1bfabef99c
Kengo Suzuki <kengoscal@gmail.com>
2019-06-08 23:09:57 +0900

    initial setup

--- main.go
+++ main.go
@@ -3,10 +3,38 @@ package main // import "github.com/you/hello"
 import (
 	"fmt"
 	"golang.org/x/oauth2"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"github.com/pelletier/go-toml"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var oauthConfig oauth2.Config
+var port string
+
+func init() {
+	tomlInBytes, err := ioutil.ReadFile("config.toml")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("failed reading a toml file: %v", err.Error()))
+	}
+
+	config, err := toml.LoadBytes(tomlInBytes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("failed parsing a toml file: %v", err.Error()))
+	}
+
+	port = strconv.FormatInt(config.Get("port").(int64), 10)
+}
 
 func main() {
+	// assuming administrative requests from frontend
+	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type"})
+	// Options is for CORS preflight
+	allowedMethods := handlers.AllowedMethods([]string{http.MethodOptions, http.MethodGet, http.MethodPost})
+
 	fmt.Println("hoge")
 }
