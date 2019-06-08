f5e8c5fa44666a827ec08117606a2868bb5169b4
Kengo Suzuki <kengoscal@gmail.com>
2019-06-08 22:41:15 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ken5scal/oauth-az
+
+require golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
---
+++ main.go
@@ -0,0 +1,12 @@
+package main // import "github.com/you/hello"
+
+import (
+	"fmt"
+	"golang.org/x/oauth2"
+)
+
+var oauthConfig oauth2.Config
+
+func main() {
+	fmt.Println("hoge")
+}
