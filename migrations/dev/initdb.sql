d0a3aed765b81f1b088cc093022de94ef22f2848
Kengo Suzuki <kengoscal@gmail.com>
2019-08-21 00:39:33 +0900

    change db dialect from mysql to postgres

--- migrations/dev/initdb.sql
+++ migrations/dev/initdb.sql
@@ -1,17 +1,19 @@
 
 -- +migrate Up
+CREATE TYPE client_status as ENUM('developing','published','suspended','deleted');
+CREATE TYPE client_type as ENUM('confidential', 'public');
 CREATE TABLE IF NOT EXISTS clients (
     client_id    VARCHAR(255)   NOT NULL UNIQUE,
     client_secret_hash VARCHAR(255) UNIQUE,
     salt               VARCHAR(255) UNIQUE,
-    status ENUM('developing','published','suspended','deleted') default 'developing' NOT NULL,
-    client_type ENUM('confidential', 'public') NOT NULL,
+    status client_status default 'developing' NOT NULL,
+    type client_type NOT NULL,
     revision_ver  INTEGER default 1 NOT NULL,
     PRIMARY KEY  (client_id)
 );
 
-ALTER TABLE clients ADD INDEX index_client_on_client_id(client_id);
-ALTER TABLE clients ADD INDEX index_client_on_revision_ver(revision_ver);
+CREATE INDEX index_client_on_client_id ON clients(client_id);
+CREATE INDEX index_client_on_revision_ver ON clients(revision_ver);
 
 CREATE TABLE IF NOT EXISTS authz (
     authorization_id    INTEGER   NOT NULL UNIQUE,
@@ -25,12 +27,12 @@ CREATE TABLE IF NOT EXISTS authz (
     PRIMARY KEY (authorization_id, client_id, user_id),
     FOREIGN KEY(client_id) references clients(client_id) on update cascade,
     FOREIGN KEY(revision_ver) references clients(revision_ver)  on update cascade on delete restrict
-#     FOREIGN KEY(user_id) references client(user_id) on update cascade on delete set null,
+--      FOREIGN KEY(user_id) references client(user_id) on update cascade on delete set null,
 );
 
 
 -- +migrate Down
 DROP TABLE authz;
-drop INDEX index_client_on_client_id ON clients;
-drop INDEX index_client_on_revision_ver ON clients;
+drop INDEX index_client_on_client_id;
+drop INDEX index_client_on_revision_ver;
 DROP TABLE clients;
